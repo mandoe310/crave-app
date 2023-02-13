73f6933be8a5e570591983ee7d692dd6fa4f346a
kylerwan <kyler.y.wang@gmail.com>
2023-02-13 13:23:04 -0800

    full CRUD for the Place entity

---
+++ main_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('about/', views.about, name='about'),
+    path('places/', views.places_index, name='index'),
+    path('places/create/', views.PlaceCreate.as_view(),name='places_create'),
+    path('places/<int:place_id>/', views.places_detail, name='detail'),
+    path('places/<int:pk>/update/', views.PlaceUpdate.as_view(),name='places_update'),
+    path('places/<int:pk>/delete/', views.PlaceDelete.as_view(),name='places_delete'),
+
+]
