2823bc5a5824f66f02d798a41c0c861285c82b50
mandoe310 <mandoe1@yahoo.com>
2023-02-13 16:29:00 -0800

    adds autentication and photo

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from datetime import date
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 RATING=(
     (1, '1'),
@@ -18,13 +17,22 @@ class Place(models.Model):
   location = models.CharField(max_length=100)
   notes = models.TextField(max_length=2500)
   rating = models.IntegerField(
-    choices=RATING,
-    )
+    choices=RATING,)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   
-#   user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
   def __str__(self):
     return f'{self.name} ({self.id})'
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={'place_id': self.id})
+
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for place_id: {self.place_id} @{self.url}"
+
+
